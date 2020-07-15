29c1dcb3ad911f17f4df486d7b7f4685c577206b
Benjamin Young <64510379+Byoung23@users.noreply.github.com>
2020-07-14 21:16:28 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+# Daily-planner (Home Work 5)
+
+Deployed: https://byoung23.github.io/daily-planner/
+
+
+## Table of contents
+
+- [General info](#general-info)
+- [Requirements](#Requirements)
+
+
+
+## General info
+
+The purpose of this website is to help orginze your daily events in one source. You can enter text describe your event and save by clicking the save button. 
+
+This project utilizes a full MERN stack in order to CRUD data and display it realtime on visualizations. 
+- JQuery
+- HTML
+- CSS
+- Javascript
+
+
+## Requirements 
+
+- Functional, deployed application. (COMPLETED)
+
+- GitHub repository with a unique name and a README describing project. (COMPLETED)
+
+- The application displays timeblocks for standard business hours (9 a.m. to 5 p.m.).  (COMPLETED)
+
+- Each timeblock contains an input field and save button.  (COMPLETED)
+
+- Clicking a timeblock's "Save" button stores the input text in local storage, allowing the text to persist when the application is refreshed. (COMPLETED)
+
+- The current day is displayed at the top of the calendar.(COMPLETED)
+
+- Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
+
+```
+GIVEN that an employee adds events to a specific hour in a calendar (COMPLETED)
+
+WHEN the employee clicks the save button  (COMPLETED)
+
+THEN events are saved in the timeblock for that hour (COMPLETED)
+```
+
---
+++ script.js
@@ -0,0 +1,71 @@
+// Set the clock
+var clock = $('#clock');
+function update() {
+	$('#clock').html(moment().format('MMMM DD YYYY H:mm:ss'));
+}
+setInterval(update, 1000);
+
+// Create all necessary elements
+var hourArry = ["9:00 a.m.", "10:00 a.m.", "11:00 a.m.", "12:00 p.m.", "1:00 p.m.", "2:00 p.m.", "3:00 p.m.", "4:00 p.m.", "5:00 p.m.", ]
+var container = $(".container");
+
+var date = moment("12/25/1995", "MM-DD-YYYY");
+
+// FOR MULTI DAY PLANNER ADD NAVIGATE FUNCTIONS: PREV, TODAY, NEXT, AND SEARCH FIELD
+
+
+// Build timeblocks
+// FOR MULTI DAY PLANNER ADD FUNCTION TO BE CALLED ON LOAD AND WHEN NAVIGATE BUTTONS ARE PRESSED
+for (let i = 0; i < hourArry.length; i++) {
+    
+    var row = $("<div>");
+    
+    row.addClass("row");
+    
+    container.append(row);
+
+	// Time block with text of hour
+	var timeBlock = $("<div>");
+    
+    timeBlock.addClass("time-block col-md-2");
+    
+    timeBlock.text(hourArry[i]);
+    
+    row.append(timeBlock);
+
+	var textArea = $("<textarea>");
+	textArea.addClass("textarea col-md-9");
+	textArea.attr('id', hourArry[i]);
+
+    
+	textArea.attr("placeholder", "Add to schedule");
+	if (localStorage.getItem(hourArry[i]) !== null) {
+		textArea.text(localStorage.getItem(hourArry[i]))
+		
+	}
+	row.append(textArea);
+	
+
+	// Change style based on past, present, future
+	if (i+9 < parseInt(moment().format('HH'))) {
+		textArea.addClass("past");
+	} 	else if (i+9 === parseInt(moment().format('HH'))) {
+		textArea.addClass("present");
+	} 	else if (i+9 > parseInt(moment().format('HH'))) {
+		textArea.addClass("future");
+	}
+
+	// Save button
+	var saveBtn = $("<button>");
+	saveBtn.addClass("saveBtn col-md-1");
+	saveBtn.text("Save");
+	row.append(saveBtn);
+
+}
+
+$(document).ready(function () {
+	$(".saveBtn").click(function () {
+		localStorage.setItem($(this).parent().children("textarea").attr("id") , $(this).parent().children("textarea").val());
+	});
+});
+
